35cfd773d5cf4bb64e2f41bf2028118eb27abd75
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2021-02-09 23:39:30 -0500

    wip select day

--- src/Automate.hs
+++ src/Automate.hs
@@ -1,7 +1,11 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+
 module Automate where
 
 
-import Data.Text (unpack)
+import Data.Text (Text, pack, unpack)
+import Data.Time.Calendar (Day, toGregorian)
 import Test.WebDriver.Class
 import Test.WebDriver.Commands
 
@@ -13,3 +17,14 @@ getHouseBills :: WebDriver m => Config -> m Agenda
 getHouseBills cfg = do
   openPage (unpack (unHouseFormUrl (houseFormUrl cfg)))
   return (error "todo")
+
+
+daySelector :: Day -> Text
+daySelector day =
+  let (_, month, dayOfMonth) = toGregorian day
+      monthText = monthToText month
+  in "a[title=\"" <> monthText <> " " <> pack (show dayOfMonth) <> "\"]"
+
+
+monthToText :: Int -> Text
+monthToText = error "todo"
