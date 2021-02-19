bd111cfc8783e910f68f2b41b65164497f2923f6
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2021-02-18 23:31:58 -0500

    wip ui

--- src/Client.hs
+++ src/Client.hs
@@ -1,7 +1,11 @@
 {-# LANGUAGE ConstraintKinds            #-}
 {-# LANGUAGE CPP                        #-}
+{-# LANGUAGE DeriveGeneric              #-}
 {-# LANGUAGE DerivingStrategies         #-}
+{-# LANGUAGE ExtendedDefaultRules       #-}
+{-# LANGUAGE FlexibleInstances          #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE OverloadedLabels           #-}
 {-# LANGUAGE OverloadedStrings          #-}
 {-# LANGUAGE TypeApplications           #-}
 
@@ -9,11 +13,18 @@
 module Main where
 
 
+import           Prelude                     hiding (div)
+
+import           Control.Lens                ()
 import           Control.Monad.Catch         (MonadThrow, MonadCatch)
 import           Control.Monad.IO.Class      (MonadIO (liftIO))
+import           Control.Monad.Trans.Class   (MonadTrans (lift))
+import           Data.Generics.Labels        ()
 import           Data.Proxy                  (Proxy (Proxy))
+import           Data.Text                   (Text)
 import           Data.Time.Calendar          (Day)
 import           Data.Time.Clock             (getCurrentTime, UTCTime (utctDay), addUTCTime, nominalDay)
+import           GHC.Generics                (Generic)
 import           Language.Javascript.JSaddle (runJSaddle)
 import           Servant.API
 import           Shpadoinkle                 (shpadoinkle, Html,
@@ -22,11 +33,14 @@ import           Shpadoinkle                 (shpadoinkle, Html,
                                               askJSM, UnliftIO (UnliftIO))
 import           Shpadoinkle.Backend.ParDiff (runParDiff)
 import           Shpadoinkle.Html
+import           Shpadoinkle.Lens            (onRecord)
 import           Shpadoinkle.Router.Client   (ClientM, client, runXHR)
 import           Shpadoinkle.Run             (live, runJSorWarp)
 
 import           Types
 import           Types.Api
+
+default (Text)
 
 
 class TestifyEffects m where
@@ -49,6 +63,10 @@ instance TestifyEffects UIM where
   getAgenda = UIM . runXHR . getAgendaM
   testify = UIM . runXHR . testifyM
 
+instance ( Monad m, MonadTrans t, TestifyEffects m ) => TestifyEffects (t m) where
+  getAgenda = lift . getAgenda
+  testify = lift . testify
+
 
 getAgendaM :: Day -> ClientM AgendaResult
 testifyM :: Submission -> ClientM TestifyResult
@@ -70,14 +88,22 @@ data ViewModel =
   , vmPositions :: Positions
   , vmPersons   :: [PersonalInfo]
   }
-  deriving (Eq, Show)
+  deriving (Eq, Generic, Show)
 
 emptyViewModel :: Day -> ViewModel
 emptyViewModel day = ViewModel day (Agenda mempty) (Positions mempty) []
 
 
-view :: ViewModel -> Html m ViewModel
-view _ = "hello world"
+daySelect :: Day -> Html m Day
+daySelect _ = div [] []
+
+
+view :: Effects m => ViewModel -> Html m ViewModel
+view model =
+  div
+    [class' "app"]
+    [ onRecord #vmDay $ daySelect (vmDay model)
+    ]
 
 
 app :: JSM ()
