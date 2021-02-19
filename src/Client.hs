91d15ba1ed66b6e9b19af5e202e0ee1d0c93d9f5
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2021-02-18 23:15:33 -0500

    wip ui

--- src/Client.hs
+++ src/Client.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE ConstraintKinds            #-}
 {-# LANGUAGE CPP                        #-}
 {-# LANGUAGE DerivingStrategies         #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
@@ -9,15 +10,20 @@ module Main where
 
 
 import           Control.Monad.Catch         (MonadThrow, MonadCatch)
-import           Control.Monad.IO.Class      (MonadIO)
-import           Data.Proxy
-import           Data.Time.Calendar
+import           Control.Monad.IO.Class      (MonadIO (liftIO))
+import           Data.Proxy                  (Proxy (Proxy))
+import           Data.Time.Calendar          (Day)
+import           Data.Time.Clock             (getCurrentTime, UTCTime (utctDay), addUTCTime, nominalDay)
+import           Language.Javascript.JSaddle (runJSaddle)
 import           Servant.API
-import           Shpadoinkle                 (Html, JSM, MonadJSM)
+import           Shpadoinkle                 (shpadoinkle, Html,
+                                              JSM, MonadJSM, newTVarIO,
+                                              MonadUnliftIO (askUnliftIO),
+                                              askJSM, UnliftIO (UnliftIO))
 import           Shpadoinkle.Backend.ParDiff (runParDiff)
 import           Shpadoinkle.Html
-import           Shpadoinkle.Router.Client
-import           Shpadoinkle.Run             (live, runJSorWarp, simple)
+import           Shpadoinkle.Router.Client   (ClientM, client, runXHR)
+import           Shpadoinkle.Run             (live, runJSorWarp)
 
 import           Types
 import           Types.Api
@@ -34,6 +40,11 @@ newtype UIM a = UIM { runUIM :: JSM a }
   deriving newtype MonadJSM
 #endif
 
+instance MonadUnliftIO UIM where
+  askUnliftIO = do
+    c <- askJSM
+    return $ UnliftIO $ \(UIM m) -> runJSaddle @IO c m
+
 instance TestifyEffects UIM where
   getAgenda = UIM . runXHR . getAgendaM
   testify = UIM . runXHR . testifyM
@@ -44,12 +55,38 @@ testifyM :: Submission -> ClientM TestifyResult
 getAgendaM :<|> testifyM = client (Proxy @Api)
 
 
-view :: () -> Html m ()
+type Effects m =
+  ( MonadThrow m
+  , MonadCatch m
+  , MonadJSM m
+  , TestifyEffects m
+  )
+
+
+data ViewModel =
+  ViewModel
+  { vmDay       :: Day
+  , vmAgenda    :: Agenda
+  , vmPositions :: Positions
+  , vmPersons   :: [PersonalInfo]
+  }
+  deriving (Eq, Show)
+
+emptyViewModel :: Day -> ViewModel
+emptyViewModel day = ViewModel day (Agenda mempty) (Positions mempty) []
+
+
+view :: ViewModel -> Html m ViewModel
 view _ = "hello world"
 
 
 app :: JSM ()
-app = simple runParDiff () view getBody
+app = do
+  now <- liftIO getCurrentTime
+  let tomorrow = utctDay (addUTCTime nominalDay now)
+      initialModel = emptyViewModel tomorrow
+  model <- newTVarIO (emptyViewModel tomorrow)
+  shpadoinkle runUIM runParDiff initialModel model view getBody
 
 
 dev :: IO ()
